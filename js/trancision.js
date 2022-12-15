a8babe04fca41f060eacd7206e459b03aeb5c4bb
bravodaniel12 <danigobra2020@gmail.com>
2022-12-15 11:20:53 -0500

    modifi

--- js/trancision.js
+++ js/trancision.js
@@ -5,12 +5,27 @@ box3.addEventListener("click", function() {
     if(box3.classList.contains("box3-old")){
         box3.classList.add("box3-new");
         box3.classList.remove("box3-old");
-    }
+    }}
     else{
         box3.classList.remove("box3-new");
         box3.classList.add("box3-old");
-    }
+    }else {
+        let box4 = document.getElementById("box4");
+        let box4_1 = document.getElementById("box4_1");
         
-    }
-
-)
+        box4.addEventListener("click", function() {
+            if(box4_1.classList.contains("box-4-off"))
+            (box4_1.classList.add("box-4-on"))
+            (box4_1.classList.remove("box-4-off"))
+            (box4.classList.add("box-4-green"))
+            (box4.classList.remove("box-4-gray"))
+        }
+        else{
+            box4_1.classList.remove("box-4-on")
+            box4_1.classList.add("box-4-off")
+            box4.classList.remove("box-4-green")
+            box4.classList.add("box-4-gray")
+        }
+        
+    
+    })
