70099ab89b3ec80e6e8b3991dd49ab9cbbc80566
unknown <SENA@PITAPRTNNFSP028.FormacionSena.RED>
2023-03-23 12:04:08 -0500

    modi

---
+++ js/carrito.js
@@ -0,0 +1,6 @@
+let imgPrincipal = document.getElementById("imgPrincipal");
+let mainBox = document-getElementById("mainBox");
+
+imgPrincipal.addEventListener("click", function(){
+  mainBox.classlist.add("fondo-oscuro")
+});
