dae39d2dfb76ce547872d44ae1cf94499210a81a
lucas <lasknfaidbafaso@gamil.com>
2023-03-30 09:07:15 -0500

    modificaciones

--- js/carrito.js
+++ js/carrito.js
@@ -1,6 +1,11 @@
-let imgPrincipal = document.getElementById("imgPrincipal");
-let mainBox = document-getElementById("mainBox");
+let imgBox = document.getElementById("imgBox");
+let imgMini1 = document.getElementById("imgMini1");
+let imgMini2 = document.getElementById("imgMini2");
+let imgMini3 = document.getElementById("imgMini3");
+let imgMini4 = document.getElementById("imgMini4");
+
+
 
 imgPrincipal.addEventListener("click", function(){
   mainBox.classlist.add("fondo-oscuro")
-});
+}); 
