d82b4171facc24baad4f8596ac1fc46337aed9c0
david18012003 <bravocarmen264@gmail.com>
2023-05-04 11:41:57 -0500

    modi

---
+++ js/slider.js
@@ -0,0 +1,7 @@
+let leftArrow = document.getElementById("leftArrow");
+let imgcanada = document.getElementById("imgcanada")
+
+leftArrow.addEventListener("click", function(){
+    leftArrow.classList.remove("canada-camry");
+    leftArrow.classList.remove("canada-camry-hidden");
+});
