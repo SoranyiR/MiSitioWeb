413382eca97616b1b5770e738ef12f8d2c5020f5
david18012003 <bravocarmen264@gmail.com>
2023-05-11 11:14:27 -0500

    modificaciones

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,7 @@
-let leftArrow = document.getElementById("leftArrow");
-// let imgcanada = document.getElementById("imgcanada")
+// let leftArrow = document.getElementById("leftArrow");
+// // let imgcanada = document.getElementById("imgcanada")
 
-let rightRow = document.getElementById("rightRow");
+// let rightRow = document.getElementById("rightRow");
 
 // imgcanada.addEventListener("click", function () {
 //     imgcanada.classList.add("canada-camry");
@@ -12,7 +12,6 @@ let rightRow = document.getElementById("rightRow");
 //     // imgcanada.classList.add("canada-camry-hidden");
 // });
 
-//--------------------------------------------------------//
 
 
 // let imgcanada2 = document.getElementById("imgcanada2")
@@ -26,45 +25,68 @@ let rightRow = document.getElementById("rightRow");
 //     // imgcanada.classList.add("canada-camry-hidden");
 // });
 
-let row = document.querySelectorAll(".row");
-let img = document.querySelectorAll(".img");
-let cont = 0;
-let contT = 1
-// for (let i = 0; i <= contT; i++) {
-//     cont = cont +1;
-//     contT = contT +1;
-//     console.log("CONT " + cont)
-//     for (let index = 0; index < cont; index++) {
-//         row[i].addEventListener("click", function () {
-//             if (i == 0) {
-//                 img[index].classList.add("canada-camry");
-//                 console.log(index)
-//             }
-//             // imgcanada.classList.add("canada-camry-hidden");
-//         });
+// let row = document.querySelectorAll(".row");
+// let img = document.querySelectorAll(".img");
+// let cont = 0;
+// let contT = 1
+// // for (let i = 0; i <= contT; i++) {
+// //     cont = cont +1;
+// //     contT = contT +1;
+// //     console.log("CONT " + cont)
+// //     for (let index = 0; index < cont; index++) {
+// //         row[i].addEventListener("click", function () {
+// //             if (i == 0) {
+// //                 img[index].classList.add("canada-camry");
+// //                 console.log(index)
+// //             }
+// //             // imgcanada.classList.add("canada-camry-hidden");
+// //         });
+// //     }
+
+// // }
+
+
+// // for (let i = 0; i < row.length; i++) {
+
+// //     // for (let index = 0; index < cont; index++) {
+// //     row[i].addEventListener("click", function () {
+// //         for (let index = 0; index < img.length; index++) {
+// //             if (i == 0) {
+// //                 img[cont].classList.add("canada-camry");
+// //                 console.log("index" + i)
+// //             }
+// //             if (i == 1) {
+// //                 // img[index].classList.add("canada-camry");
+// //                 console.log("index" + i)
+// //             }
+// //             // imgcanada.classList.add("canada-camry-hidden");
+// //         }
+// //         cont = cont + 1
+// //     });
+
+
+// // }
+// //     // }
+
+// let leftArrow = document.getElementById("leftArrow");
+// let imgNoruegaCamry = document.getElementsBy("imgcanada");
+// let imgNoruegaPlaya = document.getElementById("imgcanada2");
+// let imgNoruegaRav4 = document.getElementById("imgcanada3");
+
+// leftArrow.addEventListener("click", function(){
+//     if (imgNoruegaCamry.classlist.contains("canada1-camry")){
+//         imgNoruegaCamry.classlist.remove("canada1-camry");
+//         imgNoruegaCamry.classlist.add("canada-camry-hidden");
+//         imgNoruegaPlaya.classlist.add("canada2-camry");
+//         imgNoruegaPlaya.classList.remove("img-Noruega-Playa-hidden-right");
+//     }
+//     else if(imgNoruegaPlaya.classList.contains("img-Noruega-Playa")) {
+//         imgNoruegaPlaya.classList.remove("img-Noruega-Playa");
+//         imgNoruegaPlaya.classList.add("img-Noruega-Playa-hidden-left");
+//         imgNoruegaRav4.classList.add("img-Noruega-rav4");
+//         imgNoruegaRav4.classList.remove("img-Noruega-rav4-hidden-right");
 //     }
 
-// }
+    
+// });
 
-
-for (let i = 0; i < row.length; i++) {
-
-    // for (let index = 0; index < cont; index++) {
-    row[i].addEventListener("click", function () {
-        for (let index = 0; index < img.length; index++) {
-            if (i == 0) {
-                img[cont].classList.add("canada-camry");
-                console.log("index" + i)
-            }
-            if (i == 1) {
-                // img[index].classList.add("canada-camry");
-                console.log("index" + i)
-            }
-            // imgcanada.classList.add("canada-camry-hidden");
-        }
-        cont = cont + 1
-    });
-
-
-}
-    // }
