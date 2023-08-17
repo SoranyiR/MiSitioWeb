6ed0164041e9f0900b6a27b1fdf544f23c1918f2
estebangaviria2006 <munozleandro343@gmail.com>
2023-08-17 11:44:22 -0500

    modi

---
+++ js/homecenter.js
@@ -0,0 +1,13 @@
+let ana1 = document.getElementById("ana1") 
+
+ana1.addEventListener('click',function(){
+alert('Hey Alerta Wey!');
+}
+);   
+
+let exi1 = document.getElementById("exi1") 
+
+exi1.addEventListener('click',function(){
+alert('Hey Alerta Wey!');
+}
+);  
