84ecf225cb0870832a7bf488abad84d1dfd23209
SoranyiR <soranyiibata@gmail.com>
2023-11-16 11:37:31 -0500

    modi

--- js/estrellas.js
+++ js/estrellas.js
@@ -16,49 +16,49 @@ grafica.addEventListener("click", () => {
         estrella5.setAttribute("class", "fa-regular fa-star");
         
     }
-    else if (numero >= 0.3 && numero <= 0.7) {
+    else if (numero >= 0.3 && numero < 0.8) {
         estrella1.setAttribute("class", "fa-solid fa-star-half");
         estrella2.setAttribute("class", "fa-regular fa-star");
         estrella3.setAttribute("class", "fa-regular fa-star");
         estrella4.setAttribute("class", "fa-regular fa-star");
         estrella5.setAttribute("class", "fa-regular fa-star");
     }
-    else if (numero >= 0.8 && numero <= 1.2) {
+    else if (numero >= 0.8 && numero < 1.3) {
         estrella1.setAttribute("class", "fa-solid fa-star");
         estrella2.setAttribute("class", "fa-regular fa-star");
         estrella3.setAttribute("class", "fa-regular fa-star");
         estrella4.setAttribute("class", "fa-regular fa-star");
         estrella5.setAttribute("class", "fa-regular fa-star");
     }
-    else if (numero >= 1.3 && numero <= 1.7) {
+    else if (numero >= 1.3 && numero < 1.8) {
         estrella1.setAttribute("class", "fa-solid fa-star");
         estrella2.setAttribute("class", "fa-solid fa-star-half");
         estrella3.setAttribute("class", "fa-regular fa-star");
         estrella4.setAttribute("class", "fa-regular fa-star");
         estrella5.setAttribute("class", "fa-regular fa-star");
     }
-    else if (numero >= 1.8 && numero <= 2.2) {
+    else if (numero >= 1.8 && numero < 2.3) {
         estrella1.setAttribute("class", "fa-solid fa-star");
         estrella2.setAttribute("class", "fa-solid fa-star");
         estrella3.setAttribute("class", "fa-regular fa-star");
         estrella4.setAttribute("class", "fa-regular fa-star");
         estrella5.setAttribute("class", "fa-regular fa-star");
     }
-    else if (numero >= 2.3 && numero <= 2.7) {
+    else if (numero >= 2.3 && numero < 2.8) {
         estrella1.setAttribute("class", "fa-solid fa-star");
         estrella2.setAttribute("class", "fa-solid fa-star");
         estrella3.setAttribute("class", "fa-solid fa-star-half");
         estrella4.setAttribute("class", "fa-regular fa-star");
         estrella5.setAttribute("class", "fa-regular fa-star");
     }
-    else if (numero >= 2.8 && numero <= 3.2) {
+    else if (numero >= 2.8 && numero < 3.3) {
         estrella1.setAttribute("class", "fa-solid fa-star");
         estrella2.setAttribute("class", "fa-solid fa-star");
         estrella3.setAttribute("class", "fa-solid fa-star");
         estrella4.setAttribute("class", "fa-regular fa-star");
         estrella5.setAttribute("class", "fa-regular fa-star");
     }
-    else if (numero >= 3.3 && numero <= 3.7) {
+    else if (numero >= 3.3 && numero < 3.8) {
         estrella1.setAttribute("class", "fa-solid fa-star");
         estrella2.setAttribute("class", "fa-solid fa-star");
         estrella3.setAttribute("class", "fa-solid fa-star");
@@ -66,7 +66,7 @@ grafica.addEventListener("click", () => {
         estrella5.setAttribute("class", "fa-regular fa-star");
 
     }
-    else if (numero >= 3.8 && numero <= 4.2) {
+    else if (numero >= 3.8 && numero < 4.3) {
         estrella1.setAttribute("class", "fa-solid fa-star");
         estrella2.setAttribute("class", "fa-solid fa-star");
         estrella3.setAttribute("class", "fa-solid fa-star");
@@ -74,7 +74,7 @@ grafica.addEventListener("click", () => {
         estrella5.setAttribute("class", "fa-regular fa-star");
 
     }
-    else if (numero >= 4.3 && numero <= 4.7) {
+    else if (numero >= 4.3 && numero < 4.8) {
         estrella1.setAttribute("class", "fa-solid fa-star");
         estrella2.setAttribute("class", "fa-solid fa-star");
         estrella3.setAttribute("class", "fa-solid fa-star");
@@ -94,3 +94,4 @@ grafica.addEventListener("click", () => {
     }
 
 });
+
